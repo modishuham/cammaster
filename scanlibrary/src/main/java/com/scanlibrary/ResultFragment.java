375aa19d645f97a934c6d3424061edeec2b8cbaa
shubham.modi <shubham.modi@tothenew.com>
2020-07-19 19:04:37 +0530

    commit for UI change

--- scanlibrary/src/main/java/com/scanlibrary/ResultFragment.java
+++ scanlibrary/src/main/java/com/scanlibrary/ResultFragment.java
@@ -6,7 +6,8 @@ import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -89,7 +90,7 @@ public class ResultFragment extends Fragment {
                 try {
                     if (transformed == null)
                         transformed = original;
-                    transformedBR = adjustedContrast(transformed, progress);
+                    transformedBR = changeBitmapContrastBrightness(transformed, 1 + progress, 0);
                 } catch (Exception e) {
                     transformed = original;
                     scannedImageView.setImageBitmap(original);
@@ -115,7 +116,7 @@ public class ResultFragment extends Fragment {
                 try {
                     if (transformed == null)
                         transformed = original;
-                    transformedBR = changeBrightness(transformed, progress);
+                    transformedBR = changeBitmapContrastBrightness(transformed, 1, progress + 1);
                 } catch (Exception e) {
                     transformed = original;
                     scannedImageView.setImageBitmap(original);
@@ -450,95 +451,26 @@ public class ResultFragment extends Fragment {
         transformedBR = null;
     }
 
-    public static Bitmap changeBrightness(Bitmap src, int value) {
-        int width = src.getWidth();
-        int height = src.getHeight();
-        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
-        int A, R, G, B;
-        int pixel;
-        // scan through all pixels
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                // get pixel color
-                pixel = src.getPixel(x, y);
-                A = Color.alpha(pixel);
-                R = Color.red(pixel);
-                G = Color.green(pixel);
-                B = Color.blue(pixel);
-                // increase/decrease each channel
-                R += value;
-                if (R > 255) {
-                    R = 255;
-                } else if (R < 0) {
-                    R = 0;
-                }
-                G += value;
-                if (G > 255) {
-                    G = 255;
-                } else if (G < 0) {
-                    G = 0;
-                }
-                B += value;
-                if (B > 255) {
-                    B = 255;
-                } else if (B < 0) {
-                    B = 0;
-                }
-                // apply new pixel color to output bitmap
-                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
-            }
-        }
-        // return final image
-        return bmOut;
-    }
-
-    private Bitmap adjustedContrast(Bitmap src, double value) {
-        int width = src.getWidth();
-        int height = src.getHeight();
-        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
-        // create a canvas so that we can draw the bmOut Bitmap from source bitmap
-        Canvas c = new Canvas();
-        c.setBitmap(bmOut);
-        // draw bitmap to bmOut from src bitmap so we can modify it
-        c.drawBitmap(src, 0, 0, new Paint(Color.BLACK));
-        // color information
-        int A, R, G, B;
-        int pixel;
-        // get contrast value
-        double contrast = Math.pow((100 + value) / 100, 2);
-        // scan through all pixels
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                // get pixel color
-                pixel = src.getPixel(x, y);
-                A = Color.alpha(pixel);
-                // apply filter contrast for every channel R, G, B
-                R = Color.red(pixel);
-                R = (int) (((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if (R < 0) {
-                    R = 0;
-                } else if (R > 255) {
-                    R = 255;
-                }
-                G = Color.green(pixel);
-                G = (int) (((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if (G < 0) {
-                    G = 0;
-                } else if (G > 255) {
-                    G = 255;
-                }
-                B = Color.blue(pixel);
-                B = (int) (((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if (B < 0) {
-                    B = 0;
-                } else if (B > 255) {
-                    B = 255;
-                }
-                // set new pixel color to output bitmap
-                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
-            }
-        }
-        return bmOut;
+    /**
+     * @param bmp        input bitmap
+     * @param contrast   0..10 1 is default
+     * @param brightness -255..255 0 is default
+     * @return new bitmap
+     */
+    public static Bitmap changeBitmapContrastBrightness(Bitmap bmp, float contrast, float brightness) {
+        ColorMatrix cm = new ColorMatrix(new float[]
+                {
+                        contrast, 0, 0, 0, brightness,
+                        0, contrast, 0, 0, brightness,
+                        0, 0, contrast, 0, brightness,
+                        0, 0, 0, 1, 0
+                });
+        Bitmap ret = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
+        Canvas canvas = new Canvas(ret);
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(cm));
+        canvas.drawBitmap(bmp, 0, 0, paint);
+        return ret;
     }
 
     protected synchronized void showProgressDialog(String message) {
