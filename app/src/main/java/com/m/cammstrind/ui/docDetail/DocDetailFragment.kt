bc104e9bf821e4b4fd229873d936328d9b532dbf
shubham.modi <shubham.modi@tothenew.com>
2020-07-22 19:58:45 +0530

    commit for doc share and delete added

--- app/src/main/java/com/m/cammstrind/ui/docDetail/DocDetailFragment.kt
+++ app/src/main/java/com/m/cammstrind/ui/docDetail/DocDetailFragment.kt
@@ -2,6 +2,7 @@ package com.m.cammstrind.ui.docDetail
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +16,8 @@ import java.io.File
 
 class DocDetailFragment : Fragment() {
 
-    private var docPath:String = ""
+    private var docPath: String = ""
+    private var docName: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,27 +29,52 @@ class DocDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        tv_doc_name_detail.text = arguments?.getString("docName")
+        docName = arguments?.getString("docName").toString()
+        tv_doc_name_detail.text = docName
         val image =
             arguments?.getParcelable<Bitmap>("docImage")
         docPath = arguments?.getString("docPath").toString()
-        iv_doc_detail.setImageBitmap(image)
+        //iv_doc_detail.setImageBitmap(image)
 
         btn_share.setOnClickListener {
             shareDoc()
         }
+
+        btn_delete.setOnClickListener {
+            deleteDoc()
+        }
+
+        val mediaStorageDir: String =
+            "" + requireContext().getExternalFilesDir(null) + "/CamMaster"
+        val mFolder = File(mediaStorageDir, docName)
+        if (mFolder.exists()) {
+            iv_doc_detail.setImageBitmap(BitmapFactory.decodeFile(mFolder.absolutePath))
+        }
     }
 
     private fun shareDoc() {
-        val intent = Intent(Intent.ACTION_SEND)
-        val uri = FileProvider.getUriForFile(
-            requireContext(),
-            "com.m.cammstrind.FileProvider",
-            File( docPath)
-        )
-        intent.putExtra(Intent.EXTRA_STREAM, uri)
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        intent.type = "image/*"
-        startActivity(intent)
+        try {
+            val intent = Intent(Intent.ACTION_SEND)
+            val uri = FileProvider.getUriForFile(
+                requireContext(),
+                "com.scanlibrary.provider1",
+                File(docPath)
+            )
+            intent.putExtra(Intent.EXTRA_STREAM, uri)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            intent.type = "image/*"
+            startActivity(Intent.createChooser(intent, "Share"))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun deleteDoc() {
+        val mediaStorageDir: String =
+            "" + requireContext().getExternalFilesDir(null) + "/CamMaster"
+        val mFolder = File(mediaStorageDir, docName)
+        if (mFolder.exists()) {
+            mFolder.delete()
+        }
     }
 }
